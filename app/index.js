35b1960275957861712135bb8cdee06894f4de77
caragea.denis1806 <caragea.denis1806@gmail.com>
2024-03-06 20:22:13 +0200

    dasdas

--- app/index.js
+++ app/index.js
@@ -2,8 +2,8 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => 
-  res.send(`Docker container web app running on external port ${process.env.PORT || 9999}`));
+  res.send(`Docker container web app running on external port ${process.env.PORT || 80}`));
 
-const server = app.listen(9999, () => {
+const server = app.listen(80, () => {
   console.log(`Server is running on internal port 9999`);
 });
